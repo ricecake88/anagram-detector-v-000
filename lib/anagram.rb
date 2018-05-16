1c7eda66eb18d9027a72dd11d109506f65d39e95
Grace Shih <vocalise@gmail.com>
2018-05-16 03:34:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(possible_anagrams) #returns all matches in array
+    anagrams = []
+    characters_to_match = @word.chars.sort(&:casecmp).join
+    possible_anagrams.each_with_index do |possible, i|
+      if possible.chars.sort(&:casecmp).join == characters_to_match
+         anagrams << possible_anagrams[i]
+      end
+    end
+    anagrams
+  end
+end
