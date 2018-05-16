9376981e236d759e8f500d2daf35c23d87cd6ff0
Grace Shih <vocalise@gmail.com>
2018-05-16 03:38:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,8 @@ class Anagram
   end
   
   def match(possible_anagrams) #returns all matches in array
-    anagrams = []
-    characters_to_match = @word.chars.sort(&:casecmp).join
-    possible_anagrams.each_with_index do |possible, i|
-      if possible.chars.sort(&:casecmp).join == characters_to_match
-         anagrams << possible_anagrams[i]
-      end
+    possible_anagrams.select do |possible|
+      @word.split("").sort == possible.split("").sort
     end
-    anagrams
   end
 end
